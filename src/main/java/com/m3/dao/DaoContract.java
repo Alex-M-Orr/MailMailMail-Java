189593bc0a619462e6d7140af20c037eb9c0d1e9
Robert <rob89porto@gmail.com>
2020-11-02 21:18:20 -0500

    user model class and dao contract

---
+++ src/main/java/com/m3/dao/DaoContract.java
@@ -0,0 +1,15 @@
+package com.m3.dao;
+
+import java.util.List;
+
+public interface DaoContract<T, I> {
+	List<T> findAll();
+
+	T findById(I i);
+
+	T update(T t);
+
+	T save(T t);
+
+	T delete(I i);
+}
--- src/main/java/com/m3/model/User.java
+++ src/main/java/com/m3/model/User.java
@@ -1,5 +1,82 @@
 package com.m3.model;
 
+import java.time.LocalDateTime;
+
 public class User {
+	private int userId;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private LocalDateTime dateJoined;
+	private String photo;
+
+	public User(int userId, String username, String password, String firstName, String lastName,
+			LocalDateTime dateJoined, String photo) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateJoined = dateJoined;
+		this.photo = photo;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDateTime getDateJoined() {
+		return dateJoined;
+	}
+
+	public void setDateJoined(LocalDateTime dateJoined) {
+		this.dateJoined = dateJoined;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
 
 }
