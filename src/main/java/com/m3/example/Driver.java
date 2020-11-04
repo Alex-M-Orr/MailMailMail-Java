462172693d516a82b6c792ba0e2d83329292d5b2
Alex Orr <orr.m.alexander@gmail.com>
2020-11-04 17:01:47 -0600

    pushing changes to driver and userDaoTest

--- src/main/java/com/m3/example/Driver.java
+++ src/main/java/com/m3/example/Driver.java
@@ -9,13 +9,17 @@ public class Driver {
 	public static void main(String[] args) {
 		LocalDateTime lt = LocalDateTime.now();
 		UserDao ud = new UserDao();
-		User u1 = new User(0, "robert3", "asdfpass", "ghjJay", "ghjgSchwin", lt, "ghjgasdfasdfasdfasdf");
-		User u2 = new User(0, "John3", "asdfspass", "hfghjghjJasdf", "ghjghasdfas", lt, "ghjghasdfasdfasdfasdf");
-//		ud.save(u1);
-//		ud.save(u2);
-
-		System.out.println(u1);
-		System.out.println(u1.getFirstName());
+		User u1 = new User(0, "aorr917@gmail.com", "password", "Alex", "Orr", lt, "sdkjfgnskjdfngf", null, null, null);
+		User u2 = new User(0, "robert.porto@revature.net", "robert", "Robert", "Porto", lt, "jdhfxbggsdfgehtfyc", null, null, null);
+		User u3 = new User(0, "jordan.hunnicutt@revature.net", "dummyJordan", "Jordan", "Hunnicut", lt, "sdkjfgnskjduydfbgfngf", null, null, null);
+		
+		ud.save(u1);
+		ud.save(u2);
+		ud.save(u3);
+		
+		System.out.println(ud.findAll());
+		
+		
 
 	}
 
