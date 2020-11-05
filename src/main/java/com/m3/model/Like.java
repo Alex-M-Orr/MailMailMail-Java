63888b13b35873f08d457e2c0ce41156dcb27c06
Alex Orr <orr.m.alexander@gmail.com>
2020-11-05 14:08:51 -0600

    help

---
+++ src/main/java/com/m3/model/Like.java
@@ -0,0 +1,104 @@
+package com.m3.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Like {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="postId", referencedColumnName="id")
+	private Post post;
+	
+	@ManyToOne
+	@JoinColumn(name="commentId", referencedColumnName = "id")
+	private Comment comment;
+	
+	@ManyToOne
+	@JoinColumn(name="authorId", referencedColumnName = "id")
+	private User author;
+	
+	@Column
+	private LocalDateTime dateCreated;
+
+	public Like(int id, Post post, Comment comment, User author, LocalDateTime dateCreated) {
+		super();
+		this.id = id;
+		this.post = post;
+		this.comment = comment;
+		this.author = author;
+		this.dateCreated = dateCreated;
+	}
+
+	public Like(Post post, Comment comment, User author, LocalDateTime dateCreated) {
+		super();
+		this.post = post;
+		this.comment = comment;
+		this.author = author;
+		this.dateCreated = dateCreated;
+	}
+	
+	public Like() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public Comment getComment() {
+		return comment;
+	}
+
+	public void setComment(Comment comment) {
+		this.comment = comment;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	@Override
+	public String toString() {
+		return "Like [id=" + id + ", post=" + post + ", comment=" + comment + ", author=" + author + ", dateCreated="
+				+ dateCreated + "]";
+	}
+
+	
+	
+}
---
+++ src/main/java/com/m3/model/Post.java
@@ -0,0 +1,86 @@
+package com.m3.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String content;
+	@Column
+	private String photo;
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User author;
+	@Column(nullable = false)
+	private LocalDateTime dateCreated;
+
+	public Post(String content, String photo, User author, LocalDateTime dateCreated) {
+		super();
+		this.content = content;
+		this.photo = photo;
+		this.author = author;
+		this.dateCreated = dateCreated;
+	}
+	public Post(int id, String content, String photo, User author, LocalDateTime dateCreated) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.photo = photo;
+		this.author = author;
+		this.dateCreated = dateCreated;
+	}
+	public Post() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", content=" + content + ", author=" + author + ", dateCreated=" + dateCreated + "]";
+	}
+	
+	
+	
+}
