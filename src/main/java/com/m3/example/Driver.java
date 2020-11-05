4941693fe6a11fb126e1b658b221885e7625d6cd
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-04 19:49:36 -0500

    files

--- src/main/java/com/m3/example/Driver.java
+++ src/main/java/com/m3/example/Driver.java
@@ -9,13 +9,13 @@ public class Driver {
 	public static void main(String[] args) {
 		LocalDateTime lt = LocalDateTime.now();
 		UserDao ud = new UserDao();
-		User u1 = new User(0, "robert3", "asdfpass", "ghjJay", "ghjgSchwin", lt, "ghjgasdfasdfasdfasdf");
-		User u2 = new User(0, "John3", "asdfspass", "hfghjghjJasdf", "ghjghasdfas", lt, "ghjghasdfasdfasdfasdf");
+//		User u1 = new User(0, "robert3", "asdfpass", "ghjJay", "ghjgSchwin", lt, "ghjgasdfasdfasdfasdf");
+//		User u2 = new User(0, "John3", "asdfspass", "hfghjghjJasdf", "ghjghasdfas", lt, "ghjghasdfasdfasdfasdf");
 //		ud.save(u1);
 //		ud.save(u2);
 
-		System.out.println(u1);
-		System.out.println(u1.getFirstName());
+//		System.out.println(u1);
+//		System.out.println(u1.getFirstName());
 
 	}
 
