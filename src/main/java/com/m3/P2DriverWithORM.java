25595b9f62d2ee00648b020690614036300c371d
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-05 22:40:45 -0500

    like dao and tests

--- src/main/java/com/m3/P2DriverWithORM.java
+++ src/main/java/com/m3/P2DriverWithORM.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.m3.dao.CommentDao;
 import com.m3.dao.LikeDao;
 import com.m3.dao.UserDao;
+import com.m3.model.Like;
 import com.m3.model.User;
 
 public class P2DriverWithORM {
@@ -15,18 +16,22 @@ public class P2DriverWithORM {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("config.xml");
 		LikeDao ld = ac.getBean(LikeDao.class);
-		CommentDao cd = ac.getBean(CommentDao.class);
+		//CommentDao cd = ac.getBean(CommentDao.class);
 		UserDao ud = ac.getBean(UserDao.class);
 		
-//		User u = new User(0, "eszrdxhfcgh", "jlisdfngkj", "sljhdfnbg", "jhlsgnnf", LocalDateTime.now(), "ishfdgbn", null, null, null);
-//		Like l = new Like();
-//		l.setDateCreated(LocalDateTime.now());
-//		ld.save(l);
+		//User u = new User(0, "eszrdxgfadsggjkhlhkjfhdgshgdghghkjfhhfcgh", "jlisdfngkj", "sljhdfnbg", "jhlsgnnf", LocalDateTime.now(), "ishfdgbn", null, null, null);
+		//ud.save(u);
+		User u = ud.findById(3);
+		Like l = new Like(0, null, null, u, LocalDateTime.now());
 		
-//		ud.save(u);
-		System.out.println(ld.findAll());
-		System.out.println(cd.findAll());
+		System.out.println(l.getAuthor());
+		
+		ld.save(l);
+		
+		//System.out.println(ld.findAll());
+		//System.out.println(cd.findAll());
 		System.out.println(ud.findAll());
+		System.out.println(ld.findAll());
 	}
 	
 }
