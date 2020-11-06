1e61aec71114a13f6dbb15426e6333d09ac6f388
Robert <rob89porto@gmail.com>
2020-11-05 21:02:16 -0500

    added PostService. Working on PostDaoTest

---
+++ src/main/java/com/m3/service/PostService.java
@@ -0,0 +1,40 @@
+package com.m3.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.m3.dao.PostDao;
+import com.m3.model.Post;
+
+@Service("postservice")
+public class PostService {
+
+	@Autowired
+	private PostDao pd;
+
+	public PostService() {
+		super();
+	}
+
+	@Autowired
+	public PostService(PostDao pd) {
+		super();
+		this.pd = pd;
+	}
+
+	public PostDao getPd() {
+		return pd;
+	}
+
+	@Autowired
+	public void setPd(PostDao pd) {
+		this.pd = pd;
+	}
+
+	public List<Post> getAllPosts() {
+		return pd.findAll();
+	}
+
+}
--- src/test/java/com/m3/dao/PostDaoTest.java
+++ src/test/java/com/m3/dao/PostDaoTest.java
@@ -4,24 +4,35 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
-import javax.transaction.Transactional;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.m3.model.Post;
+import com.m3.model.User;
 
 public class PostDaoTest {
-
+	private LocalDateTime date;
 	private PostDao pd;
-//	private PostDao pdMock;
+	private UserDao ud;
+	private User us;
+	private Post testPost;
+	private Post testPost2;
 
 	@Before
 	public void setup() {
-//		pdMock = Mockito.mock(PostDao.class);
+		date = LocalDateTime.now();
+
+		us = new User(0, "PostDaoTestUser@email.com", "PostDao", "PostDaoTestUser", "PostDaoTestUser",
+				LocalDateTime.now(), null, null, null, null);
+		ud = new UserDao();
+		ud.save(us);
+
+		testPost = new Post(1, "testContent", "testPhoto", us, date);
 		pd = new PostDao();
+		pd.save(testPost);
 
 	}
 
@@ -38,7 +49,6 @@ public class PostDaoTest {
 	}
 
 	@Test
-	@Transactional
 	public void updateTest() {
 		Post post = pd.findById(1);
 //		post.setContent("I love M3! The best up and comming Revature social media platform.");
@@ -46,8 +56,22 @@ public class PostDaoTest {
 		String cnt = post.getContent();
 
 		assertEquals(cnt, "Sorry, change this back");
-		throw new RuntimeException();
 
 	}
+
+//	@Test
+//	public void saveTest() {
+//		User us2 = new User(0, "test2@email.com", "test2", "tesfname2", "testlname2", LocalDateTime.now(), null, null,
+//				null, null);
+//
+//		Post testPost2 = new Post(0, "otherTestContent", "otherTestPhoto", us, date);
+////		System.out.println(us2);
+////		int rows = pd.findAll().size();
+//		Post test = pd.save(testPost2);
+////		int newrows = pd.findB().size();
+////		assertTrue(newrows == rows + 1);
+//		assertNotNull(test);
+//
+//	}
 
 }
