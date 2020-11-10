9f0559750001b43aed0f1fac5663208157740119
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-09 22:02:17 -0500

    fixed all users with dates

--- src/test/java/com/m3/service/LikeServiceTest.java
+++ src/test/java/com/m3/service/LikeServiceTest.java
@@ -33,7 +33,7 @@ public class LikeServiceTest {
 		u.setPassword("testPass");
 		u.setFirstName("testF");
 		u.setLastName("testL");
-		u.setDateJoined(LocalDateTime.now());
+		//u.setDateJoined(LocalDateTime.now());
 		ud = ac.getBean(UserDao.class);
 		ud.save(u);
 		Post p = new Post(0, "This is a post", "This is a photo", u, LocalDateTime.now());
