3c8c0a373833bdd9225bc40910848820b6bdac76
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-10 19:15:21 -0500

    commented out driver, readded useraspect

--- src/main/java/com/m3/aspect/UserAspect.java
+++ src/main/java/com/m3/aspect/UserAspect.java
@@ -1,23 +1,23 @@
-//package com.m3.aspect;
-//
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.After;
-//import org.aspectj.lang.annotation.AfterReturning;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@Aspect
-//public class UserAspect {
-//	
-////	@AfterReturning("execution(* *(..))")
-////	public void afterReturning(JoinPoint jp) {
-////		System.out.println("Advice after returning");
-////	}
-////	
-////	@After("execution(* *(..))")
-////	public void finishedPainting(JoinPoint jp) {
-////		System.out.println("The painter has completed another masterpiece");
-////	}
-//	
-//}
+package com.m3.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class UserAspect {
+	
+	@AfterReturning("execution(* *(..))")
+	public void afterReturning(JoinPoint jp) {
+		System.out.println("Advice after returning");
+	}
+	
+	@After("execution(* *(..))")
+	public void finishedPainting(JoinPoint jp) {
+		System.out.println("The painter has completed another masterpiece");
+	}
+	
+}
