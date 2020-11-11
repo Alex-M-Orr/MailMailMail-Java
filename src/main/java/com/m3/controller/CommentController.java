f71abff3dcb4178c2df5c7355050893f69a99d2b
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-10 20:20:06 -0500

    javadoc stuff, added hibernate config current session prop

--- src/main/java/com/m3/controller/CommentController.java
+++ src/main/java/com/m3/controller/CommentController.java
@@ -16,23 +16,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.m3.model.Comment;
 import com.m3.model.CommentBuilt;
 import com.m3.service.CommentService;
-
+/**
+ * 
+ * <h1>CommentController</h1>
+ * <p>The comment controller is used to get information about a comment from the front end based on different request mappings.</p>
+ * <p>This component uses annotations to indicate that it is a controller, that it uses cross origin references, that it has request mappings, and that it has connected beans</p>
+ * 
+ * @authors Alex Orr, Enoch Cho, Jordan Hunnicutt, Robert Porto, Tyrone Veneracion
+ *
+ */
 @Controller
 @CrossOrigin
 @RequestMapping
 public class CommentController {
-
+	/**
+	 * This field is a CommentService, which is a component used to communicate between this controller and the repository.
+	 */
 	private CommentService cs;
 	
+	/**
+	 * <p>The getLs method retrieves the CommentService field.</p>
+	 * 
+	 * @return CommentService cs
+	 */
 	public CommentService getCs() {
 		return cs;
 	}
 	
+	/**
+	 * 
+	 * <p>The setCs method sets the CommentService field based on a CommentService parameter.</p>
+	 * The autowired tag is used so Spring creates a bean wiring to the CommentService class.
+	 * 
+	 * @param CommentService cs
+	 */
 	@Autowired
 	public void setCs(CommentService cs) {
 		this.cs = cs;
 	}
 	
+	/**
+	 * <p>The getAll method returns a list of CommentBuilts.</p>
+	 * It gets a list of Comments from the database and rebuilds them into a format that can be more easily returned.
+	 * 
+	 * @return List<CommentBuilt> builtComments
+	 */
 	@GetMapping("/commentAll.app")
 	public @ResponseBody List<CommentBuilt> getAll(){
 		List<Comment> comments = cs.getAllComments();
